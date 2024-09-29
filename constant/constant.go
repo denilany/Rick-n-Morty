1fa48ca8ec2f9d6d116189ac0eb9b9a452b584ad
danyonyi <denilanyonyi1@gmail.com>
2024-09-30 01:04:43 +0300

    Set the variables that will remain the same, such as the API endpoints

---
+++ constant/constant.go
@@ -0,0 +1,8 @@
+package apiconfig  // or another appropriate name
+
+const (
+    BaseURL   = "https://rickandmortyapi.com/api/"
+    Character = "character"
+    Location  = "location"
+    Episode   = "episode"
+)
