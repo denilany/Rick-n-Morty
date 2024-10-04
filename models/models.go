af5fc9d264bf0363982802fbb99e807c71b5fa62
John <johneliud4@gmail.com>
2024-10-04 23:42:19 +0300

    Fix: JSON property name from Human to species to match JSON format of API data.

--- models/models.go
+++ models/models.go
@@ -1,39 +1,38 @@
 package models
 
 type CharacterResponse struct {
-	Info ResponseInfo `json:"info"`
+	Info   ResponseInfo         `json:"info"`
 	Result []ResponseCharacters `json:"results"`
 }
 
 type ResponseInfo struct {
-	Count int `json:"count"`
-	Pages int `json:"pages"`
+	Count    int    `json:"count"`
+	Pages    int    `json:"pages"`
 	NextPage string `json:"next"`
 	PrevPage string `json:"prev"`
 }
 
 type ResponseCharacters struct {
-	ID int `json:"id"`
-	Name string `json:"name"`
-	Status string `json:"status"`
-	Species string `json:"Human"`
-	Type string `json:"type"`
-	Gender string `json:"gender"`
-	Origin CharacterOrigin `json:"origin"`
-	Location CharacterLocation `json:"location"`
-	Image string `json:"image"`
-	Episode []string `json:"episode"`
-	CharacterURL string `json:"url"`
-	Created string `json:"created"`
+	ID           int               `json:"id"`
+	Name         string            `json:"name"`
+	Status       string            `json:"status"`
+	Species      string            `json:"species"`
+	Type         string            `json:"type"`
+	Gender       string            `json:"gender"`
+	Origin       CharacterOrigin   `json:"origin"`
+	Location     CharacterLocation `json:"location"`
+	Image        string            `json:"image"`
+	Episode      []string          `json:"episode"`
+	CharacterURL string            `json:"url"`
+	Created      string            `json:"created"`
 }
 
 type CharacterOrigin struct {
-	Name string `json:"name"`
+	Name      string `json:"name"`
 	OriginURL string `json:"url"`
 }
 
 type CharacterLocation struct {
-	Name string `json:"name"`
+	Name        string `json:"name"`
 	LocationURL string `json:"url"`
 }
-
