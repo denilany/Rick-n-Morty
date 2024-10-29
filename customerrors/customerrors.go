4116cb1e9d76408ed0ca9a4b68f3104b4c04a3df
johnodhiambo0 <johneliud4@gmail.com>
2024-10-29 12:39:27 +0300

    chore: define function that parses a HTML error template along with the error handler function and displays on the website.

--- customerrors/customerrors.go
+++ customerrors/customerrors.go
@@ -1,10 +1,31 @@
 package customerrors
 
-import "net/http"
+import (
+	"html/template"
+	"log"
+	"net/http"
+	"path/filepath"
+)
 
 type ErrorPage struct {
 	StatusCode int
 	Message    string
+}
+
+func ServeErrorPage(w http.ResponseWriter, errorPage ErrorPage) {
+	tmplPath := filepath.Join("templates", "error-page.html")
+	tmpl, err := template.ParseFiles(tmplPath)
+	if err != nil {
+		http.Error(w, "An Unexpected Error Occurred. Try Again Later", http.StatusInternalServerError)
+		log.Printf("Error parsing template %s: %v\n", tmplPath, err)
+		return
+	}
+	w.WriteHeader(errorPage.StatusCode)
+
+	if err := tmpl.Execute(w, errorPage); err != nil {
+		http.Error(w, "An Unexpected Error Occurred. Try Again Later", http.StatusInternalServerError)
+		log.Printf("Error executing template: %v\n", err)
+	}
 }
 
 func NotFoundHandler(w http.ResponseWriter, r *http.Request) {
