d536a79a8505bf0b61b1fe9918bc946d84e2c3f1
johnodhiambo0 <johneliud4@gmail.com>
2024-10-29 14:50:40 +0300

    update: function GetCharacters to additionally accept an http.ResponseWriter as a parameter to enable error instances defined within it align with ServeErrorPage function.

--- api/character.go
+++ api/character.go
@@ -7,10 +7,11 @@ import (
 	"time"
 
 	apiconfig "github.com/denilany/Rick-n-Morty/constant"
+	"github.com/denilany/Rick-n-Morty/customerrors"
 	"github.com/denilany/Rick-n-Morty/models"
 )
 
-func GetCharacters(page string) (*models.CharacterResponse, error) {
+func GetCharacters(w http.ResponseWriter, page string) (*models.CharacterResponse, error) {
 	url := apiconfig.BaseURL + apiconfig.Character
 
 	if page != "" {
@@ -23,6 +24,10 @@ func GetCharacters(page string) (*models.CharacterResponse, error) {
 
 	req, err := http.NewRequest("GET", url, nil)
 	if err != nil {
+		customerrors.ServeErrorPage(w, customerrors.ErrorPage{
+			StatusCode: http.StatusInternalServerError,
+			Message:    "An Unexpected Error Occurred. Try Again Later",
+		})
 		return nil, fmt.Errorf("failed to create new request: %w", err)
 	}
 
@@ -32,17 +37,29 @@ func GetCharacters(page string) (*models.CharacterResponse, error) {
 	// Make request using the client
 	resp, err := client.Do(req)
 	if err != nil {
+		customerrors.ServeErrorPage(w, customerrors.ErrorPage{
+			StatusCode: http.StatusInternalServerError,
+			Message:    "An Unexpected Error Occurred. Try Again Later",
+		})
 		return nil, fmt.Errorf("request failed: %w", err)
 	}
 
 	defer resp.Body.Close()
 
 	if resp.StatusCode != http.StatusOK {
+		customerrors.ServeErrorPage(w, customerrors.ErrorPage{
+			StatusCode: http.StatusBadRequest,
+			Message:    "Bad Request",
+		})
 		return nil, fmt.Errorf("unexpected status code: %d", resp.StatusCode)
 	}
 
 	var characterResponse models.CharacterResponse
 	if err := json.NewDecoder(resp.Body).Decode(&characterResponse); err != nil {
+		customerrors.ServeErrorPage(w, customerrors.ErrorPage{
+			StatusCode: http.StatusInternalServerError,
+			Message:    "An Unexpected Error Occurred. Try Again Later",
+		})
 		return nil, fmt.Errorf("failed to decode response: %w", err)
 	}
 
