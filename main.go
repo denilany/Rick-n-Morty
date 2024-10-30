fb2915a596f6f508ca998da0510c130539185e77
johnodhiambo0 <johneliud4@gmail.com>
2024-10-30 08:42:31 +0300

    feat: introduce a default handler that serves an 404 error for any unmatched route passed on the URL while redirecting '/' to 'characters' thereby fixing default behavior by Go for parsing a 404 error to any undefined route and instead use the 404 error handler function.

--- main.go
+++ main.go
@@ -4,14 +4,26 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/denilany/Rick-n-Morty/customerrors"
 	"github.com/denilany/Rick-n-Morty/route"
 )
 
 func main() {
 	http.HandleFunc("/characters", route.CharacterHandler)
-	
+
 	fs := http.FileServer(http.Dir("static"))
 	http.Handle("/static/", http.StripPrefix("/static/", fs))
+
+	// A default handler to pass a 404 error for any unmatched or undefined route
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		if r.URL.Path != "/" {
+			customerrors.NotFoundHandler(w, r)
+			log.Println("Page not found.")
+			return
+		}
+		// Optionally redirect "/" to /characters
+		http.Redirect(w, r, "/characters", http.StatusSeeOther)
+	})
 
 	log.Println("server starting at http://localhost:8080/characters")
 	log.Fatal(http.ListenAndServe(":8080", nil))
